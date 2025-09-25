a46618cba8ec9b0bce347d5dc38ec2f656f88a42
xianyu-sheng <2107644732@qq.com>
2025-09-25 10:21:26 +0800

    "买卖股票的最佳时机||-动态规划思想“

--- 122.买卖股票的最佳时机-ii.cpp
+++ 122.买卖股票的最佳时机-ii.cpp
@@ -8,12 +8,23 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
-        for (size_t i = 1; i < prices.size(); ++i) {
-            if (prices[i] > prices[i-1])
-                ans += prices[i] - prices[i-1];  // 只要今天比昨天贵就“买卖”
+        if (prices.empty()) return 0;
+
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2));
+
+        // 初始化
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < n; ++i) {
+            // 不持有：要么昨天就不持，要么今天卖了
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            // 持有：要么昨天就持，要么今天买了
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
-        return ans;
+
+        return dp[n-1][0];
     }
 };
 // @lc code=end
