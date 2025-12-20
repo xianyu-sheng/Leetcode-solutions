f0efdab4b669cc245fc3e032ce49cc6a1ca298c4
xianyu-sheng <2107644732@qq.com>
2025-12-20 11:54:24 +0800

    “84.柱状图中最大的矩形【使用双数组维护矩形左右两边边界】”

--- 84.柱状图中最大的矩形.cpp
+++ 84.柱状图中最大的矩形.cpp
@@ -47,5 +47,50 @@ public:
         return result;
     }
 };
+//方法2
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        /*
+            题意理解：在这个题中我们其实可以采用一个中心扩散法，就是假设每一个柱子我们都可以将
+            其设为高度，然后看以它为高度，我们的矩形面积到底有多大，可以有多大，然后我们又可以
+            根据木桶原理，决定我们矩形的高度的就是这个矩形中的柱形的最低的那个柱形，因此为了能够
+            计算出以当前柱子为最低的哪个柱子的矩形面积，我们就需要保持住在这个矩形中，当前的这个矩形
+            中当前的柱形的高度最低，这时候我们需要维护这个矩形满足这个木桶原理的边界，因此我们使用
+            两个数组来维护两边的边界
+        */
+        int n=heights.size();
+        if(n==0)    return 0;
+
+        vector<int> minleftindex(n);
+        vector<int> minrightindex(n);
+        
+        //然后进行左右两边第一个比自己小的柱形下标的统计
+        minleftindex[0]=-1;
+        for(int i=1;i<n;i++){
+            int t=i-1;
+            while(t>=0 && heights[t]>=heights[i]){
+                t=minleftindex[t];
+            }
+            minleftindex[i]=t;
+        }
+        minrightindex[n-1]=n;
+        for(int i=n-2;i>=0;i--){
+            int t=i+1;
+            while(t<n && heights[t]>=heights[i]){
+                t=minrightindex[t];
+            }
+            minrightindex[i]=t;
+        }
+        //然后遍历所有柱子 计算最大的矩形的面积
+        int rel=INT_MIN;
+        for(int i=0;i<n;i++){
+            int h=heights[i];
+            int w=minrightindex[i]-minleftindex[i]-1;//去掉两侧比自己小的柱形，只留这块自己是最小的宽度
+            rel=max(rel,h*w);
+        }
+        return rel;
+    }
+};
 // @lc code=end
 
